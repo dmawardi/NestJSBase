a089e7e3397c4a9ad23247a1a41a1297709bbd0e
Dannar Mawardi <dmawardi@gmail.com>
2021-04-28 14:53:07 +0700

    Basic Service with mock DB complete

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,15 +8,21 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  // The below is added for the coffees service
+  //   private:available in this class only; readonly: best practice, same effect;
+  // then, declare instance of class
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   // Return all coffees
   @Get()
   //   Query parameter below allows you to use pagination: coffees?limit=20&offset=10
   findAll(@Query() paginationQuery) {
     const { limit, offset } = paginationQuery;
-    return `This action returns all coffees. Limit: ${limit}. Offset: ${offset}`;
+    return this.coffeesService.findAll();
   }
 
   //   Return details of single coffee using nested URL
@@ -27,26 +33,26 @@ export class CoffeesController {
   //   }
   // The version below is updated to only use one parameter
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffees`;
+    return this.coffeesService.findOne(id);
   }
 
   //   Post Decorator for post requests updating whole record routes
   @Post()
   //   Using the Body Decorator, you can access the body of the request
   create(@Body() body) {
-    return body;
+    return this.coffeesService.create(body);
   }
 
   // Patch decorator for partially updating record routes
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   //   Delete Decorator for deletion request routes
   @Delete(':id')
   //   Methods for this route below
   delete(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
