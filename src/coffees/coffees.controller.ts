23c8f0bd307a2bc485ab79cb0bd0cbae9b9d4728
Dannar Mawardi <dmawardi@gmail.com>
2021-04-28 13:15:09 +0700

    implemented query parameters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,17 +1,25 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   // Return all coffees
   @Get()
-
-  //   Using the @Res decorator, you are able to access the response as it's used in Express
-  //   This is not the ideal way to use though, so use with caution
-  findAll() {
-    return 'This action returns all coffees';
+  //   Query parameter below allows you to use pagination: coffees?limit=20&offset=10
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffees. Limit: ${limit}. Offset: ${offset}`;
   }
 
-  //   Return details of single coffee
+  //   Return details of single coffee using nested URL
   @Get(':id')
   //   The Param decorator below, allows us to to use params in our method below
   //   findOne(@Param() params ) {
@@ -22,10 +30,23 @@ export class CoffeesController {
     return `This action returns #${id} coffees`;
   }
 
+  //   Post Decorator for post requests updating whole record routes
   @Post()
-
   //   Using the Body Decorator, you can access the body of the request
   create(@Body() body) {
     return body;
+  }
+
+  // Patch decorator for partially updating record routes
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  //   Delete Decorator for deletion request routes
+  @Delete(':id')
+  //   Methods for this route below
+  delete(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
