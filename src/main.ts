a7d9c2aa147435e5f6c1b3b4f6774474fcbc5828
Dannar Mawardi <dmawardi@gmail.com>
2021-04-28 18:35:17 +0700

    forbidding malicious/incorrect requests

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Added for data validation. Uses class-validator and class-transformer packages
   // This allows us to add validation to our DTO's
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Makes sure all unwanted properties are automatically stripped out
+      // This is possible through nest's dto system effortlessly
+      whitelist: true,
+      // If any non whitelisted data is detected, automatically forbid from proceeding
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
