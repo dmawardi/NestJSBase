ce28de28f11b37857fa85118e3ea8da6582a98f3
Dannar Mawardi <dmawardi@gmail.com>
2021-04-29 16:58:09 +0700

    Connected to Docker postgresDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    // import TypeOrm with config
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      // the default for postgres sql
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      // loads modules automatically instead of having to provide entities array
+      autoLoadEntities: true,
+      // Ensures our TypeOrm entities will be synced with database
+      // Great for development. Disable for production
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
