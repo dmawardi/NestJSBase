db439f953acd61c65ad1ab9dac0b9615df59c6fa
Dannar Mawardi <dmawardi@gmail.com>
2021-05-05 15:36:55 +0700

    Succeeded to load .env variables to connect to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,20 +6,24 @@ import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    // Enables use of .env variables
+    ConfigModule.forRoot(),
     CoffeeRatingModule,
     CoffeesModule,
     // import TypeOrm (using forRoot as it's root file) with config
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+
       // the default for postgres sql
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       // loads modules automatically instead of having to provide entities array
       autoLoadEntities: true,
       // Ensures our TypeOrm entities will be synced with database
