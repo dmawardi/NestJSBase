0042f5525118d23ba8c1cf29849610860f2b2e70
Dannar Mawardi <dmawardi@gmail.com>
2021-05-11 10:42:55 +0700

    changed response to place data in data property

--- src/common/interceptors/wrap-response.interceptor.ts
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -5,7 +5,7 @@ import {
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class WrapResponseInterceptor implements NestInterceptor {
@@ -13,6 +13,9 @@ export class WrapResponseInterceptor implements NestInterceptor {
     console.log('Before...');
     // Uses call handler to handle route (next.handle())
     // This can be used further to manipulate the stream
-    return next.handle().pipe(tap((data) => console.log('After...', data)));
+    // return next.handle().pipe(tap((data) => console.log('After...', data)));
+    // The call handler can also be used to map/reshape data
+    // Below adds the data as its own key/value in response
+    return next.handle().pipe(map((data) => ({ data })));
   }
 }
